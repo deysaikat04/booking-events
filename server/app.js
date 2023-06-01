7724757ca2c9dcf3dfb9e01f3e99ef60a4c64fa1
Saikat Dey <30950554+deysaikat04@users.noreply.github.com>
2023-06-01 12:46:29 +0530

    feat: added simple event and mutation logic

--- server/app.js
+++ server/app.js
@@ -5,44 +5,67 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
-app.use(
-   bodyParser.json()
-);
+// TEMP
+const events = [];
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-   res.send("hello there!!");
+	res.send("hello there!!");
 });
 
 app.use(
-   "/graphql",
-   graphqlHttp({
-      schema: buildSchema(`
-          type RootQuery {
-              events: [String!]!
-          }
-  
-          type RootMutation {
-              createEvent(name: String): String
-          }
-  
-          schema {
-              query: RootQuery
-              mutation: RootMutation
-          }
+	"/graphql",
+	graphqlHttp({
+		schema: buildSchema(`
+			type Event {
+				_id: ID!
+				title: String!
+				description: String!
+				price: Float!
+				date: String!
+			}
+
+			input EventInput {
+				title: String!
+				description: String!
+				price: Float!
+				date: String!
+			}
+
+			type RootQuery {
+				events: [Event!]!
+			}
+
+			type RootMutation {
+				createEvent(eventInput: EventInput): Event
+			}
+
+			schema {
+				query: RootQuery
+				mutation: RootMutation
+			}
       `),
-      rootValue: {
-         events: () => {
-            return ["Romantic Cooking", "Sailing", "All-Night Coding"];
-         },
-         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
-         },
-      },
-      graphiql: true,
-   })
+		rootValue: {
+			events: () => {
+				return events;
+			},
+			createEvent: ({ eventInput }) => {
+				const event = {
+					_id: Math.random().toString(),
+					title: eventInput.title,
+					description: eventInput.description,
+					price: eventInput.price,
+					date: eventInput.date
+				}
+				events.push(event)
+				return event;
+			},
+		},
+		graphiql: true,
+	})
 );
 
 app.listen(5500, () => {
-   console.log("Server started....");
+	console.log("Server started....");
 });
